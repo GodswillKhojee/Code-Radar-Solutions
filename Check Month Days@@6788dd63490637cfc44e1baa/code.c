8e16332caaaebdf438fdf41bf80a1bc1c8640e69
GodswillKhojee <ardiumwing@gmail.com>
2025-02-10 18:41:26 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -1,64 +1,62 @@
 #include <stdio.h>
 
+
 int main()
 {
-    int main()
+    int a;
+    scanf("%d",&a);
+
+    switch(a)
     {
-        int a;
-        scanf("%d",&a);
+        case 1:
+            printf("31");
+            break;
 
-        switch(a)
-        {
-            case 1:
-                printf("31");
-                break;
+        case 2:
+            printf("28");
+            break;
 
-            case 2:
-                printf("28");
-                break;
+        case 3:
+            printf("31");
+            break;
 
-            case 3:
-                printf("31");
-                break;
+        case 4:
+            printf("30");
+            break;
+        
+        case 5:
+            printf("31");
+            break;
 
-            case 4:
-                printf("30");
-                break;
-            
-            case 5:
-                printf("31");
-                break;
+        case 6:
+            printf("30");
+            break;
 
-            case 6:
-                printf("30");
-                break;
+        case 7:
+            printf("31");
+            break;
+        
+        case 8:
+            printf("31");
+            break;
 
-            case 7:
-                printf("31");
-                break;
-            
-            case 8:
-                printf("31");
-                break;
+        case 9:
+            printf("30");
+            break;
 
-            case 9:
-                printf("30");
-                break;
+        case 10:
+            printf("31");
+            break;
 
-            case 10:
-                printf("31");
-                break;
+        case 11:   
+            printf("30");
+            break;
 
-            case 11:   
-                printf("30");
-                break;
+        case 12:
+            printf("31");
+            break;
 
-            case 12:
-                printf("31");
-                break;
-
-            default:
-                printf("Invalid month");
-        }
+        default:
+            printf("Invalid month");
     }
 }
